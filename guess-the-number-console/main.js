0015e10202c25fe424ed7af55a6ede8deeea4877
Pain2Miee <nguyen.thaomy.p13com@gmail.com>
2021-09-28 16:58:25 +0200

    update javascript

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -2,17 +2,27 @@ console.log('Hello User!')
 console.log('Try to guess the mystery number.')
 console.log('Use "submit(x)" to submit a number.')
 
-const mysteryNumber = 45
+const mysteryNumber = Math.round(Math.random() * 100)
+
 
 const submit = (x) => {
-    if(x < mysteryNumber) {
+    if (x < mysteryNumber) {
         console.log('Too small')
-    }
-    if(x > mysteryNumber) {
+        document.body.innerHTML = '<h1>Too small<h1>'
+        document.body.style.backgroundColor = '#ff0000'
+    } 
+    if (x > mysteryNumber) {
         console.log('Too big')
+        document.body.innerHTML = '<h1>Too big<h1>'
+        document.body.style.backgroundColor = '#0008ff'
     }
-        if(x == mysteryNumber) {
+    if (x == mysteryNumber) {
         console.log('Hurray')
+        document.body.innerHTML = '<h1>Hurray<h1>'
+        document.body.style.backgroundColor = '#6eff00'
     }
 }
 
+const cheat = () => {
+    console.log(`The mystery number is ${mysteryNumber}`)
+}
